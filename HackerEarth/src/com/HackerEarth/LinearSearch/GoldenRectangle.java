a244f0665fe90d5aee951e31c9b7c60c8306775a
mephi007 <roysumit949@gmail.com>
2019-05-29 02:26:43 +0530

    modified GoldenRectangle

--- HackerEarth/src/com/HackerEarth/LinearSearch/GoldenRectangle.java
+++ HackerEarth/src/com/HackerEarth/LinearSearch/GoldenRectangle.java
@@ -19,16 +19,20 @@ public class GoldenRectangle {
             
             for(int k=0; k<a.length; k=k+2)
             {
-                double ratio = a[k] /  a[k+1];
-                double ratio1 = a[k+1]/a[k];
-                if(ratio>=1.600 && ratio<1.71)
+            	 double ratio=0;
+            	if(a[k] > a[k+1])
+            	{
+               ratio = a[k] /  a[k+1];
+            	}
+            	else
+            	{
+            		ratio = a[k+1]/a[k];
+            	}
+                if(ratio>1.599999 && ratio<1.71)
                 {
                 count++;
                 }
-                if(ratio1>=1.600 && ratio1<1.71)
-                {
-                	count++;
-                }
+                
                 System.out.println("count " +count);
                 System.out.println(k);
                 System.out.println("ratio "+ratio);
